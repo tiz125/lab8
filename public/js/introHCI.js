25b5a5927a94f1def0436aa7172d160f1ebc1172
Leoshenqingquan <wxzleo@gmail.com>
2019-02-28 11:52:55 -0800

    second

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,26 +2,34 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-  initializePage();
+    initializePage();
 })
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
- 	initGestures();
- 	initRSVPForm();
+    console.log("Page ready");
+    // initCamera();
+    initMap();
+    initGestures();
+    initRSVPForm();
 }
 
 // init jQuery gestures  
 function initGestures() {
-	// add gestures listener here
+    $(function() {
+        $(".judge-img").bind("taphold", tapholdHandler);
+
+        function tapholdHandler(event) {
+            var targetIDPrefix = event.target.id;
+            console.log("got prefix:" + targetIDPrefix);
+            $("#" + targetIDPrefix + "-bio").show();
+        }
+    });
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+    // add your code here
 }
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+    L.mapquest.key = 'QYLu4fMnPywnnsApAuLVVImtBDjUsU0D';
+
+    var map = L.mapquest.map('map', {
+        center: [32.88, -117.236],
+        layers: L.mapquest.tileLayer('map'),
+        zoom: 12,
+        zoomControl: false
+    });
+
+    L.marker([32.88, -117.236]).addTo(map);
 }
